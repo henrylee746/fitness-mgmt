8f340564fafd1b150e3da2d4e4241e4f7240f386
henrylee746 <henry.lee746@gmail.com>
2026-02-17 14:24:56 -0500

    Delete old fitness-appc3005 directory

--- fitness-appc3005/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Homepage } from "./Homepage";
-
-export default function Home() {
-  return (
-    <div className="flex flex-col h-full items-center justify-center">
-      <Homepage />
-    </div>
-  );
-}
--- fitness-appc3005/src/app/signin/page.tsx
+++
@@ -1,9 +0,0 @@
-import Login from "./SigninForm";
-
-export default function SigninPage() {
-  return (
-    <div className="h-full w-full flex flex-col justify-center items-center">
-      <Login />
-    </div>
-  );
-}
